e09383b1acc55200ec8651e876be7bab54ea86a0
Berke <69055194+fsb3rke@users.noreply.github.com>
2022-07-16 20:14:51 +0300

    Update profile.py

--- profile.py
+++ profile.py
@@ -4,32 +4,24 @@ import os
 class profile:
     def __init__(self, profile_id:str):
         self.id = profile_id
+        self.__profiles_dir = "profiles/"
     
     def create(self):
-        root_directory = os.getcwd()
-        os.chdir("profiles")
-        file_name = f"{self.id}.json"
+        file_name = f"{self.__profiles_dir}{self.id}.json"
         with open(file_name, 'w') as f:
             file_content = {"id": self.id, "money": 0}
             file_content_json = json.dumps(file_content)
             f.write(file_content_json)
-        os.chdir(root_directory)
 
     def get(self):
-        root_directory = os.getcwd()
-        os.chdir("profiles")
-        file_name = f"{self.id}.json"
+        file_name = f"{self.__profiles_dir}{self.id}.json"
         with open(file_name, "r") as f:
             file_content = f.read()
             file_content_json = json.loads(file_content)
-        os.chdir(root_directory)
         return file_content_json
 
     def change(self, new_profile:dict):
-        root_directory = os.getcwd()
-        os.chdir("profiles")
-        file_name = f"{self.id}.json"
+        file_name = f"{self.__profiles_dir}{self.id}.json"
         with open(file_name, 'w') as f:
             file_content_json = json.dumps(new_profile)
             f.write(file_content_json)
-        os.chdir(root_directory)
